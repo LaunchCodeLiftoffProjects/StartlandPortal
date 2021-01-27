90dba3e76c933351c92ee2d159e9b7a9b33ef7bf
Kirby <kafracas@gmail.com>
2021-01-27 14:55:04 -0600

    updated branch with master by adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -11,7 +12,16 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule
+=======
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserRegistrationComponent
+>>>>>>> 304402a... created user registration branch
   ],
+  imports: [ BrowserModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
