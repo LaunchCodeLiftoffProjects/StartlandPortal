1e0991eb5db6e3a38f6fa824b219d6627a54132f
Kirby <kafracas@gmail.com>
2021-01-27 17:00:01 -0600

    added form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,7 @@ import { UserRegistrationComponent } from './user-registration/user-registration
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
+  fieldErrorMessage = "";
+  passwordErrorMessage = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  validateInput (email: string, fullName: string, username: string, password: string, verifyPassword: string) {
+    if (email == '' || fullName == '' || username == '' || password == '' || verifyPassword == ''){
+      this.fieldErrorMessage = "ALL FIELDS REQUIRED!";
+    } else {
+      this.fieldErrorMessage = "";
+    }
+    if (password != verifyPassword){
+      this.passwordErrorMessage = "PASSWORDS MUST MATCH!";
+    } else {
+      this.passwordErrorMessage = "";
+    }
   }
 
 }
