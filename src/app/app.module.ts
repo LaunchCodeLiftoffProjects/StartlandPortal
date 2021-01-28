513714b714717cf5fc8322f47cfb89971415f592
Christopher Chaffin <christopher.chaffin@gmail.com>
2021-01-28 14:53:29 -0600

    initial course summary table complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CourseSummaryComponent } from './course-summary/course-summary.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { CourseSummaryComponent } from './course-summary/course-summary.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/course-summary/course-summary.component.ts
+++ src/app/course-summary/course-summary.component.ts
@@ -1,36 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-course-summary',
   templateUrl: './course-summary.component.html',
   styleUrls: ['./course-summary.component.css']
 })
-export class CourseSummaryComponent implements OnInit {
-  tableDataSrc: any;
-  tableCols: string[] = ['assignment', 'dueData', 'status'];
-  tableData: {}[] = [
-    {
-      assignment: 'Module #1',
-      dueData: 'January 1st, 2021',
-      status: 'Past Due'
-    },
-    {
-      assignment: 'Module #2',
-      dueData: 'February 1st, 2021',
-      status: 'Completed'
-    },
-    {
-      assignment: 'Module #1',
-      dueData: 'March 1st, 2021',
-      status: 'Not Complete'
-    }
-  ];
 
-  constructor() { }
-
-  ngOnInit() {
-    this.tableDataSrc = new MatTableDataSource(this.tableData);
-  }
-
+export class CourseSummaryComponent {
+  displayedColumns = ['moduleNum', 'description', 'dueDate', 'status']
+  dataSource = new MatTableDataSource(MODULE_DATA);
 }
+
+export interface Module {
+  moduleNum: number;
+  description: string;
+  dueDate: string;
+  status: string;
+}
+
+const MODULE_DATA: Module[] = [
+  {moduleNum: 1, description: 'Module 1', dueDate: 'January 1, 2021', status: 'Past Due'},
+  {moduleNum: 2, description: 'Module 2', dueDate: 'February 1, 2021', status: 'Completed'},
+  {moduleNum: 3, description: 'Module 3', dueDate: 'March 1, 2021', status: 'Not Complete'},
+  {moduleNum: 4, description: 'Module 4', dueDate: 'April 1, 2021', status: 'Not Complete'}
+];
