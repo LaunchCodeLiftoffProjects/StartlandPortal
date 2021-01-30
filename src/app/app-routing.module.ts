a22ba480d1a67709d9f93115dde4702558e654bf
Kristian <jettkristian@gmail.com>
2021-01-30 11:45:14 -0800

    updated log-in form to set up validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { LogInComponent } from './log-in/log-in.component'
 
 const routes: Routes = [
-  { path: 'user-registration', component: UserRegistrationComponent }
+  { path: 'user-registration', component: UserRegistrationComponent },
+  { path: 'log-in', component: LogInComponent }
 ];
 
 @NgModule({
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    ) { }
+
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required,Validators.email]],
+      // TODO: check password against database password.
+      password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  get fval() { return this.loginForm.controls; }
+
+  onFormSubmit(){
+    this.submitted = true;
+    // return for here if form is invalid
+    if (this.loginForm.invalid) {
+    return;
+    }
+    this.loading = true;
+    // should this route to the home page upon login?
   }
 
 }
