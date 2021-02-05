685ea38a5284022581ba0fc2e86335dba889752b
Janell Renee Johnson <docj4jesus@gmail.com>
2021-02-04 18:52:30 -0600

    added module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,23 @@ import { LogInComponent } from './log-in/log-in.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { ModulesComponent } from './modules/modules.component';
+import { Module1Component } from './module1/module1.component';
+import { Module2Component } from './module2/module2.component';
+import { Module3Component } from './module3/module3.component';
+import { Module4Component } from './module4/module4.component';
+import { Module5Component } from './module5/module5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
     UserRegistrationComponent,
-    ModulesComponent
+    ModulesComponent,
+    Module1Component,
+    Module2Component,
+    Module3Component,
+    Module4Component,
+    Module5Component
   ],
   imports: [
     BrowserModule,
