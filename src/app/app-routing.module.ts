ca470ab3823b73d0651ac0536b4f08e9425c85e8
Christopher Chaffin <christopher.chaffin@gmail.com>
2021-02-09 17:52:51 -0600

    Main Forum Page update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { ForumComponent } from './forum/forum.component';
+
 
 const routes: Routes = [
-  { path: 'user-registration', component: UserRegistrationComponent }
+  { path: 'user-registration', component: UserRegistrationComponent },
+  { path: 'forum', component: ForumComponent }
 ];
 
 @NgModule({
