82d55ad5737f8e72bf5fcb799374db5ee501ceae
Janell Renee Johnson <docj4jesus@gmail.com>
2021-02-09 20:44:59 -0600

    added youtube videos and updated individual modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { YouTubePlayerModule } from "@angular/youtube-player";
 
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
@@ -34,7 +33,6 @@ import { Module5Component } from './module5/module5.component';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
