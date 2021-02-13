ea8ad4393499d80a6fcd8311663a779042f0b643
Kirby Fracas <kafracas@gmail.com>
2021-02-13 15:50:37 -0600

    updated to add announcements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { HomeComponent } from './home/home.component';
+import { CourseSummaryComponent } from './course-summary/course-summary.component';
 
 const routes: Routes = [
   { path: 'user-registration', component: UserRegistrationComponent },
+  { path: 'course-summary', component: CourseSummaryComponent },
   { path: '', component: LogInComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' }
