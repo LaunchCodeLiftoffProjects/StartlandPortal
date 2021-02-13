f94eb27d5150640e9ca497c1fcdbb7a9caf56fac
Kirby <kafracas@gmail.com>
2021-02-13 13:41:31 -0600

    cleaned up back end code

--- src/app/node-js-jwt-auth/server.js
+++ src/app/node-js-jwt-auth/server.js
@@ -32,7 +32,7 @@ db.sequelize.sync().then(() => {
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Startland Portal application." });
 });
 
 // routes
