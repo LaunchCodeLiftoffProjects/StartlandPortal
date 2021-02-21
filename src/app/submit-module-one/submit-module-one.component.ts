b96c1a52b3b6390a66a2157266351d3eca691319
Kirby Fracas <kafracas@gmail.com>
2021-02-21 13:25:18 -0600

    added update assignment and delete assignment function

--- src/app/submit-module-one/submit-module-one.component.ts
+++ src/app/submit-module-one/submit-module-one.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { AssignmentService } from '../_services/assignment.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submit-module-one',
@@ -14,11 +15,14 @@ export class SubmitModuleOneComponent implements OnInit {
   assignments: any;
   updateMode = false;
   submitted = false;
+  thisAssignment: any;
+  updateMessage = '';
 
   constructor(
     private token: TokenStorageService,
     private assignmentService: AssignmentService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -37,6 +41,7 @@ export class SubmitModuleOneComponent implements OnInit {
           for (let i = 0; i < this.assignments.length; i++) {
             if (this.assignments[i].userId === this.currentUser.id && this.assignments[i].moduleNum === 1) {
               this.submitted = true;
+              this.thisAssignment = this.assignments[i];
             } else {
               this.submitted = false;
             }
@@ -53,26 +58,46 @@ export class SubmitModuleOneComponent implements OnInit {
       .subscribe(
         response => {
           console.log(response);
+          this.reloadPage("Assignment was added successfully!")
         },
         error => {
           console.log(error);
         });
       }
 
-      checkIfSubmitted(){
-        console.log(this.assignments)
-        for (let i = 0; i < this.assignments.length; i++) {
-          if (this.assignments[i].userId === this.currentUser.id && this.assignments[i].moduleNum === 1) {
-            this.submitted = true;
-          } else {
-            this.submitted = false;
-          }
-        }
-      }
-
       updateAssignment(){
-        console.log(this.assignments.length)
         this.updateMode = true;
       }
+
+      submitNewAssignment(newLink: string){  
+        this.assignmentService.update(this.thisAssignment.id, newLink)
+          .subscribe(
+            response => {
+              console.log(response);
+              this.updateMode = false;
+              this.reloadPage("Assignment was updated successfully!")
+            },
+            error => {
+              console.log(error);
+            });
+          }
+
+          deleteAssignment(){
+            this.assignmentService.delete(this.thisAssignment.id)
+              .subscribe(
+                response => {
+                  console.log(response);
+                  this.reloadPage("Assignment was deleted successfully!")
+                },
+                error => {
+                  console.log(error);
+                }
+              )
+          }
+
+          reloadPage(message: string) {
+            window.location.reload();
+            this.updateMessage = message;
+          }
 
 }
