0be6be267a098460856e9afefde122bcf053619b
Janell Renee Johnson <docj4jesus@gmail.com>
2021-03-01 23:07:20 -0600

    Working on function for progress bar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../_services/token-storage.service';
+import { AssignmentService } from '../_services/assignment.service';
+import { CourseSummaryComponent} from '../course-summary/course-summary.component';
 
 @Component({
   selector: 'app-progress-bar',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent implements OnInit {
+  currentUser: any;
+  assignments: any;
+  module: any;
 
-  constructor() { }
+  modules = [1,2,3,4];
+  submitted = [];
+
+  constructor(
+    private token: TokenStorageService,
+    private assignmentService: AssignmentService,
+    private moduleNum: CourseSummaryComponent
+  ) { }
 
   ngOnInit() {
-  }
+    this.currentUser = this.token.getUser();
 
-}
+    this.module = this.moduleNum;
+
+    this.assignmentService.getAll()
+      .subscribe(
+        data => {
+          this.assignments = data;
+
+          for (let i = 0; i < this.modules.length; i++) {
+
+            for (let a = 0; a < Object(data).length; a++) {
+
+              if (Object(data)[a].module === this.modules[i] && Object(data)[a].userId === this.currentUser.id) {
+                this.submitted.push(true);
+              }
+
+            }
+
+            if (this.submitted.length === this.modules[i] -1) {
+              this.submitted.push(false);
+            }
+
+          }
+
+        })
+    }
+  }
